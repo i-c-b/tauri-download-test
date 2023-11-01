1e4b73215818374bcc3c0ca8d028ba8a10b6a961
i-c-b <133848861+i-c-b@users.noreply.github.com>
2023-11-01 07:08:00 +0000

    Fix AppImage read-only filesystem with save dialog

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import { download } from "tauri-plugin-upload-api";
+import { save } from "@tauri-apps/api/dialog";
 
 let urlInputEl: HTMLInputElement | null;
 let jsMsgEl: HTMLElement | null;
 
 async function downloadJs() {
   if (jsMsgEl && urlInputEl) {
-    download(urlInputEl.value, "./download.txt", (progress, total) => {
-      (jsMsgEl as HTMLElement).textContent = `Downloaded: ${progress} of ${total}`;
-    });
+    let save_location: string | null;
+    try {
+      save_location = await save();
+    } catch (e) {
+      console.debug("exception from save dialog", e);
+      return;
+    }
+
+    if (save_location !== null) {
+      try {
+        await download(urlInputEl.value, save_location, (progress, total) => {
+          (jsMsgEl as HTMLElement).textContent = `Downloaded: ${progress} of ${total}`;
+        });
+      } catch (e) {
+        console.debug("exception from download", e);
+      }
+      (jsMsgEl as HTMLElement).textContent = `Download complete: ${save_location}`;
+    }
   }
 }
 
